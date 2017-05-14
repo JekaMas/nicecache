34ed5550a9d15d4e482bf914c315e2b7817ebbd9
Evgeny Danilenko <eugene.danilenko@lazada.com>
2017-05-14 10:46:18 +0300

    Fix after merge

--- nicecache.go
+++ nicecache.go
@@ -125,52 +125,7 @@ func (c *Cache) delete(h uint64, valueIdx int) {
 // FIXME Check locks distribution
 func (c *Cache) popFreeIndex() int {
 	if atomic.LoadInt32(c.freeCount) == 0 {
-		go func() {
-			// Если индексы иссякли, то считаем свободными процент от записей.
-			// TODO заменить на lru?
-			// TODO: подумать, что делать с истекшим ttl - надо высвобождать хотя бы частично эти записи. возможно с лимитом времени на gc
-
-			// все горутины берут значение onClearing, но только одна горутина увеличит c.onClearing
-			onClearing := atomic.LoadInt32(c.onClearing)
-			c.onClearingMutex.Lock()
-			if atomic.LoadInt32(c.onClearing) != onClearing {
-				c.onClearingMutex.Unlock()
-				return
-			}
-
-			i := 0
-			c.indexMap.Range(func(k, res interface{}) bool {
-				resIndex := res.(int)
-				v := c.index[resIndex][valueIndex]
-				c.freeIndexCh <- v
-				c.freeIndexes.Store(v, struct{}{})
-				c.indexMap.Delete(k)
-
-				i++
-				if i >= freeBatchSize {
-					return false
-				}
-				return true
-			})
-
-			atomic.AddInt32(c.onClearing, 1)
-			c.onClearingMutex.Unlock()
-
-			atomic.StoreInt32(c.freeCount, int32(i))
-
-			// Increase freeBatchSize progressive
-			var freeBatchSizeDelta int = freeBatchSize * alpha / 100
-			if freeBatchSizeDelta < 1 {
-				freeBatchSizeDelta = 1
-			}
-
-			freeBatchSize += freeBatchSizeDelta
-			if freeBatchSize > (cacheSize*maxFreeRatePercent)/100 {
-				freeBatchSize = (cacheSize * maxFreeRatePercent) / 100
-			}
-
-			return
-		}()
+		go c.clearCache(c.freeIndexCh)
 	}
 
 	var idx int
@@ -179,14 +134,60 @@ func (c *Cache) popFreeIndex() int {
 	} else {
 		c.freeIndexes.Range(func(k, res interface{}) bool {
 			idx = k.(int)
+			c.freeIndexes.Delete(idx)
+			atomic.AddInt32(c.freeCount, -1)
 			return false
 		})
 	}
-	c.freeIndexes.Delete(idx)
-
-	atomic.AddInt32(c.freeCount, -1)
 
 	return idx
+}
+
+func (c *Cache) clearCache(freeIndexCh chan int) {
+	// Если индексы иссякли, то считаем свободными процент от записей.
+	// TODO заменить на lru?
+	// TODO: подумать, что делать с истекшим ttl - надо высвобождать хотя бы частично эти записи. возможно с лимитом времени на gc
+
+	// все горутины берут значение onClearing, но только одна горутина увеличит c.onClearing
+	onClearing := atomic.LoadInt32(c.onClearing)
+	c.onClearingMutex.Lock()
+	if atomic.LoadInt32(c.onClearing) != onClearing {
+		c.onClearingMutex.Unlock()
+		return
+	}
+
+	i := 0
+	c.indexMap.Range(func(k, res interface{}) bool {
+		resIndex := res.(int)
+		v := c.index[resIndex][valueIndex]
+		freeIndexCh <- v
+		c.freeIndexes.Store(v, struct{}{})
+		c.indexMap.Delete(k)
+
+		i++
+		if i >= freeBatchSize {
+			return false
+		}
+		return true
+	})
+
+	atomic.AddInt32(c.onClearing, 1)
+	c.onClearingMutex.Unlock()
+
+	atomic.StoreInt32(c.freeCount, int32(i))
+
+	// Increase freeBatchSize progressive
+	var freeBatchSizeDelta int = freeBatchSize * alpha / 100
+	if freeBatchSizeDelta < 1 {
+		freeBatchSizeDelta = 1
+	}
+
+	freeBatchSize += freeBatchSizeDelta
+	if freeBatchSize > (cacheSize*maxFreeRatePercent)/100 {
+		freeBatchSize = (cacheSize * maxFreeRatePercent) / 100
+	}
+
+	return
 }
 
 func (c *Cache) pushFreeIndex(key int) {
